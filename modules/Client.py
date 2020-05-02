29e68ea2dcae9301ddf54b076b2ad00ca19e850f
sciueferrara <antonioferrara94@icloud.com>
2020-05-02 19:57:15 +0200

    Multiprocessing implementation

--- modules/Client.py
+++ modules/Client.py
@@ -1,5 +1,7 @@
 import numpy as np
-from time import time
+import multiprocessing
+from .Worker import WorkerLocal
+
 
 class Client:
     def __init__(self, client_id, model, train, train_user_list, validation_user_list, test_user_list):
@@ -18,6 +20,39 @@ class Client:
         prediction = {top_k[i]: top_k_score[i] for i in range(len(top_k))}
 
         return prediction
+
+    def train_one_sample(self, i, j, lr, positive_fraction, bias_reg, user_reg, positive_item_reg, negative_item_reg):
+        resulting_dic = {}
+        resulting_bias = {}
+
+        x_i = self.model.predict_one(i)
+        x_j = self.model.predict_one(j)
+        x_ij = x_i - x_j
+
+        d_loss = 1 / (1 + np.exp(x_ij))
+
+        bi = self.model.item_bias[i].copy()
+        bj = self.model.item_bias[j].copy()
+
+        self.model.item_bias[i] += lr * (d_loss - bias_reg * bi)
+        self.model.item_bias[j] += lr * (-d_loss - bias_reg * bj)
+
+        wu = self.model.user_vec.copy()
+        hi = self.model.item_vecs[i].copy()
+        hj = self.model.item_vecs[j].copy()
+
+        self.model.user_vec += lr * (d_loss * (hi - hj) - user_reg * wu)
+        self.model.item_vecs[i] += lr * (d_loss * wu - positive_item_reg * hi)
+        self.model.item_vecs[j] += lr * (d_loss * (-wu) - negative_item_reg * hj)
+
+        resulting_dic[j] = d_loss * (-wu) - negative_item_reg * hj
+        resulting_bias[j] = -d_loss - bias_reg * bj
+        if positive_fraction:
+            if np.random.choice([True, False], p=[positive_fraction, 1-positive_fraction]):
+                resulting_dic[i] = d_loss * wu - positive_item_reg * hi
+                resulting_bias[i] = d_loss - bias_reg * bi
+
+
     
     def train(self, lr, positive_fraction):
         bias_reg = 0
@@ -27,32 +62,48 @@ class Client:
         resulting_dic = {}
         resulting_bias = {}
 
-        for i, j in self.train_set.sample_user_triples():
-            x_i = self.model.predict_one(i)
-            x_j = self.model.predict_one(j)
-            x_ij = x_i - x_j
+        tasks = multiprocessing.JoinableQueue()
+        results = multiprocessing.Queue()
+        num_workers = multiprocessing.cpu_count() - 1
+        workers = [WorkerLocal(tasks, self.train_one_sample, results) for _ in range(num_workers)]
+        for w in workers:
+            w.start()
+        for pair in self.train_set.sample_user_triples():
+            tasks.put((pair, lr, positive_fraction, bias_reg, user_reg, positive_item_reg, negative_item_reg))
+        for i in range(num_workers):
+            tasks.put(None)
+        tasks.join()
+        for _ in range(self.sampler_size):
+            items, biases = results.get()
+            resulting_dic.update(items)
+            resulting_bias.update(biases)
 
-            d_loss = 1 / (1 + np.exp(x_ij))
-
-            bi = self.model.item_bias[i].copy()
-            bj = self.model.item_bias[j].copy()
-
-            self.model.item_bias[i] += lr * (d_loss - bias_reg * bi)
-            self.model.item_bias[j] += lr * (-d_loss - bias_reg * bj)
-
-            wu = self.model.user_vec.copy()
-            hi = self.model.item_vecs[i].copy()
-            hj = self.model.item_vecs[j].copy()
-
-            self.model.user_vec += lr * (d_loss * (hi - hj) - user_reg * wu)
-            self.model.item_vecs[i] += lr * (d_loss * wu - positive_item_reg * hi)
-            self.model.item_vecs[j] += lr * (d_loss * (-wu) - negative_item_reg * hj)
-
-            resulting_dic[j] = d_loss * (-wu) - negative_item_reg * hj
-            resulting_bias[j] = -d_loss - bias_reg * bj
-            if positive_fraction:
-                if np.random.choice([True, False], p=[positive_fraction, 1-positive_fraction]):
-                    resulting_dic[i] = d_loss * wu - positive_item_reg * hi
-                    resulting_bias[i] = d_loss - bias_reg * bi
+        # for i, j in self.train_set.sample_user_triples():
+        #     x_i = self.model.predict_one(i)
+        #     x_j = self.model.predict_one(j)
+        #     x_ij = x_i - x_j
+        #
+        #     d_loss = 1 / (1 + np.exp(x_ij))
+        #
+        #     bi = self.model.item_bias[i].copy()
+        #     bj = self.model.item_bias[j].copy()
+        #
+        #     self.model.item_bias[i] += lr * (d_loss - bias_reg * bi)
+        #     self.model.item_bias[j] += lr * (-d_loss - bias_reg * bj)
+        #
+        #     wu = self.model.user_vec.copy()
+        #     hi = self.model.item_vecs[i].copy()
+        #     hj = self.model.item_vecs[j].copy()
+        #
+        #     self.model.user_vec += lr * (d_loss * (hi - hj) - user_reg * wu)
+        #     self.model.item_vecs[i] += lr * (d_loss * wu - positive_item_reg * hi)
+        #     self.model.item_vecs[j] += lr * (d_loss * (-wu) - negative_item_reg * hj)
+        #
+        #     resulting_dic[j] = d_loss * (-wu) - negative_item_reg * hj
+        #     resulting_bias[j] = -d_loss - bias_reg * bj
+        #     if positive_fraction:
+        #         if np.random.choice([True, False], p=[positive_fraction, 1-positive_fraction]):
+        #             resulting_dic[i] = d_loss * wu - positive_item_reg * hi
+        #             resulting_bias[i] = d_loss - bias_reg * bi
 
         return resulting_dic, resulting_bias
--- modules/Worker.py
+++ modules/Worker.py
@@ -18,3 +18,23 @@ class Worker(multiprocessing.Process):
             self.work(self.clients, next_task)
             self.task_queue.task_done()
         return
+
+
+class WorkerLocal(multiprocessing.Process):
+    def __init__(self, task_queue, work, result_queue):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+        self.work = work
+        self.result_queue = result_queue
+
+    def run(self):
+        while True:
+            next_task = self.task_queue.get()
+            if next_task is None:
+                # Poison pill means shutdown
+                self.task_queue.task_done()
+                break
+            (i, j), lr, positive_fraction, bias_reg, user_reg, positive_item_reg, negative_item_reg = next_task
+            self.work(i, j, lr, positive_fraction, bias_reg, user_reg, positive_item_reg, negative_item_reg)
+            self.task_queue.task_done()
+        return
