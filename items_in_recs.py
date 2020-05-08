57ebc20bbc0141428794355b5359872063b6465e
sciueferrara <antonioferrara94@icloud.com>
2020-05-08 20:05:10 +0200

    Some utils

--- items_in_recs.py
+++ items_in_recs.py
@@ -42,9 +42,8 @@ def main(args):
                     for k, v in users.items():
                         items.extend(v[:10])
 
-                    print(items)
-
                     how_many = sorted([v for _, v in count_elements(items).items()], reverse=True)
+                    print(how_many)
 
                     #how_many = [int(r.split("\t")[1].replace('\n', '')) for r in f]
                     #how_many = sorted(how_many, reverse=True)
