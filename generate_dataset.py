1b93ded8dafcbe08f70136f90e0f042fc2fa8440
sciueferrara <antonioferrara94@icloud.com>
2020-05-26 12:57:48 +0200

    Use of Git LFS for datasets

--- generate_dataset.py
+++ generate_dataset.py
@@ -18,8 +18,13 @@ def main(args):
 
         # Read the dataset and prepare it for training, validation and test
         names = ['user_id', 'item_id', 'rating', 'utc']
-        df = pd.read_csv('raw_datasets/' + dataset + '.tsv', sep='\t',
-                         dtype={'rating': 'float64', 'utc': 'int64'}, header=0, names=names)
+        if args.parse_dates:
+            parse_dates = ['utc']
+            df = pd.read_csv('raw_datasets/' + dataset + '.tsv', sep='\t',
+                             dtype={'rating': 'float64', 'utc': 'str'}, parse_dates=parse_dates, header=0, names=names)
+        else:
+            df = pd.read_csv('raw_datasets/' + dataset + '.tsv', sep='\t',
+                             dtype={'rating': 'float64', 'utc': 'int64'}, header=0, names=names)
         df = df.groupby('user_id').filter(lambda x: len(x) >= 20)
         print(df.shape[0], 'interactions read')
         df, _ = utils.convert_unique_idx(df, 'user_id')
@@ -46,5 +51,6 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--datasets', nargs='+', help='Set the datasets you want to use', required=True)
+    parser.add_argument('--parse_dates', action='store_true', help='Set if UTC contains dates')
     parsed_args = parser.parse_args()
     main(parsed_args)
